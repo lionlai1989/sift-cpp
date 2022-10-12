945388d1fdc18540017c58140d550a055a236330
chihan <lionlai1989@gmail.com>
2022-10-12 10:01:43 +0200

    Add essential files

---
+++ examples/find_keypoints.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include <string>
+
+#include "image.hpp"
+#include "sift.hpp"
+
+int main(int argc, char *argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    if (argc != 2) {
+        std::cerr << "Usage: ./find_keypoints input.jpg (or .png)\n";
+        return 0;
+    }
+    // auto tmp =Eigen::MatrixXd(2,3);
+    // tmp << 1,2,3,4,5,6;
+    Image img(argv[1]);
+    img =  img.channels == 1 ? img : rgb_to_grayscale(img);
+
+    std::cout << img.width << img.height << img.size << std::endl;
+
+    std::vector<sift::Keypoint> kps = sift::find_keypoints_and_descriptors(img);
+    Image result = sift::draw_keypoints(img, kps);
+    result.save("result.jpg");
+
+    std::cout << "Found " << kps.size() << " keypoints. Output image is saved as result.jpg\n";
+    return 0;
+}
---
+++ pybind11_wrapper.cpp
@@ -0,0 +1,29 @@
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "sift.hpp"
+#include "image.hpp"
+
+namespace py = pybind11;
+
+std::vector<sift::Keypoint> computeKeypointsAndDescriptors(Eigen::MatrixXd pixels) {
+    Image input_image(pixels);
+    std::vector<sift::Keypoint> kps = sift::find_keypoints_and_descriptors(input_image);
+    return kps;
+}
+
+PYBIND11_MODULE(pybind11_wrapper, m) {
+    m.def("computeKeypointsAndDescriptors", &computeKeypointsAndDescriptors);
+    py::class_<sift::Keypoint>(m, "sift::Keypoint")
+    .def(py::init<>()) // <-- bind the default constructor
+    .def_readwrite("i", &sift::Keypoint::i)
+    .def_readwrite("j", &sift::Keypoint::j)
+    .def_readwrite("octave", &sift::Keypoint::octave)
+    .def_readwrite("scale", &sift::Keypoint::scale)
+    .def_readwrite("x", &sift::Keypoint::x)
+    .def_readwrite("y", &sift::Keypoint::y)
+    .def_readwrite("sigma", &sift::Keypoint::sigma)
+    .def_readwrite("extremum_val", &sift::Keypoint::extremum_val)
+    .def_readwrite("descriptor", &sift::Keypoint::descriptor);
+}
